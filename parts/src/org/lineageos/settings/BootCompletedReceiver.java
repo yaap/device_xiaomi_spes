5a62dd9b6cae459e2b6d31608fc8b5388ccbcdec
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-09-06 10:52:43 +0530

    spes: Introduce displayfeature color service and use MIUI color modes
    This service relays the current AOSP color mode to the Xiaomi
    displayfeature HAL, matching stock behaviour. This is necessary to
    avoid colors being reset after turning the screen off, because
    displayfeature sets color mode everytime the screen is turned on
    defaulting to Vivid mode. Having this fixed it finally allows us to use Miui color modes in AOSP.
    
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I7faa409bf7069108f5bed64488b2a7c5566cd921
    
    peridot: Link displayfeature binder to death
    
    If displayfeature HAL crashes and restarts, we must recreate our
    IDisplayFeature instance.
    
    Change-Id: Icefc248bda251126f66204e4f1ced2ae4a1601c6
    
    peridot: migrate to dislayfeature AIDL
    
    peridot: Introduce displayfeature color service and use MIUI color modes
    
    This service relays the current AOSP color mode to the Xiaomi
    displayfeature HAL, matching stock behaviour. This is necessary to
    avoid colors being reset after turning the screen off, because
    displayfeature sets color mode everytime the screen is turned on
    defaulting to Vivid mode. Having this fixed it finally allows us to use Miui color modes in AOSP.
    
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I7faa409bf7069108f5bed64488b2a7c5566cd921
    
    peridot: Link displayfeature binder to death
    
    If displayfeature HAL crashes and restarts, we must recreate our
    IDisplayFeature instance.
    
    Change-Id: Icefc248bda251126f66204e4f1ced2ae4a1601c6
    
    peridot: migrate to dislayfeature AIDL
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -30,7 +30,9 @@ import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.Display;
+import android.view.Display.HdrCapabilities;
 
+import org.lineageos.settings.display.ColorModeService;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.thermal.ThermalTileService;
 import org.lineageos.settings.refreshrate.RefreshUtils;
@@ -50,6 +52,12 @@ public class BootCompletedReceiver extends BroadcastReceiver {
                 handleBootCompleted(context);
                 break;
         }
+        if (DEBUG)
+            Log.d(TAG, "Received boot completed intent");
+        // Display
+        context.startServiceAsUser(new Intent(context, ColorModeService.class),
+                UserHandle.CURRENT);        
+        RefreshUtils.startService(context);
     }
 
     private void handleLockedBootCompleted(Context context) {
