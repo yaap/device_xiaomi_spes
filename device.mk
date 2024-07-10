baac2304f75a7572c05945a4658d95aab2704e1e
Guidix <guidixm@gmail.com>
2024-07-11 01:59:06 +0300

    spes: Drop XiaomiParts
    Moving to displayFeatures

--- device.mk
+++ device.mk
@@ -523,8 +523,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
-
-# XiaomiParts
-PRODUCT_PACKAGES += \
-    XiaomiParts
-    
