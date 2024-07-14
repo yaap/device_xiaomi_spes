896310b4df3164392eda466549b3ebf01122343f
angelomds42 <angelomds42@gmail.com>
2024-07-14 01:58:48 -0300

    spes: Drop compatibility matrix
    Let's use the common from the source
    
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,7 +111,6 @@ TARGET_OTA_ASSERT_DEVICE := spes,spesn
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml
-DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 ifeq ($(PRODUCT_NAME), lineage_spes)
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest-lineage.xml
