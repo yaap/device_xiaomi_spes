deb18ccd3d1e4cd78f8e12f8d12a5a122f12298a
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-07-15 22:08:35 +0530

    spes: Update extract-files.sh and setup-makefiles.sh
    Change-Id: I8fc468603bc710081dcb6122ec5732bc602a2412
    Signed-off-by: Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -9,7 +9,6 @@ set -e
 
 DEVICE=spes
 VENDOR=xiaomi
-INITIAL_COPYRIGHT_YEAR=2022
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
@@ -24,13 +23,12 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
-# Initialize the helper for common
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
-# Copyright headers and guards
-write_headers "${DEVICE}"
+# Warning headers and guards
+write_headers
 
-# The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
 
 # Finish
