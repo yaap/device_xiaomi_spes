4fcc47676188315fb8315a7e763690958f1916d0
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-08-05 23:21:40 +0530

    spes: extract-files: Add blob_fixup function
    Change-Id: I40cd3e0bf1c0ef9896240d22182d9e1370ae31e2
    Signed-off-by: Kajal4414 <81718060+Kajal4414@users.noreply.github.com>

--- extract-files.sh
+++ extract-files.sh
@@ -52,6 +52,17 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+            "${SIGSCAN}" -p "13 0A 00 94" -P "1F 20 03 D5" -f "${2}"
+            ;;
+        vendor/lib64/camera/components/com.qti.node.mialgocontrol.so)
+            llvm-strip --strip-debug "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
