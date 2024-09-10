ab30bf5aefdbe86bce645e79f8a9045b8b53af34
Michael Bestas <mkbestas@gmail.com>
2024-09-11 00:04:13 +0300

    spes: Shim libwvhidl and libwvdrmengine with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -231,7 +231,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey \
-    libcrypto-v33
+    libcrypto_shim
 
 # Fastbootd
 PRODUCT_PACKAGES += \
--- extract-files.sh
+++ extract-files.sh
@@ -79,6 +79,10 @@ function blob_fixup() {
             llvm-strip --strip-debug "${2}"
             grep -q "libpiex_shim.so" "${2}" || "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
             ;;
+        vendor/lib64/libwvhidl.so|vendor/lib64/mediadrm/libwvdrmengine.so)
+            [ "$2" = "" ] && return 0
+            grep -q libcrypto_shim.so "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            ;;
         vendor/lib64/hw/fingerprint.goodix.default.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --set-soname fingerprint.goodix.default.so "${2}"
@@ -90,10 +94,6 @@ function blob_fixup() {
         vendor/etc/seccomp_policy/atfwd@2.0.policy)
             [ "$2" = "" ] && return 0
             echo 'gettid: 1' >> ${2}
-            ;; 
-        vendor/lib64/libwvhidl.so)
-            "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "libcrypto-v34.so" "${2}"
-            "${PATCHELF}" --set-soname "libcrypto-v34.so" "${2}"
             ;;
         *)
             return 1
