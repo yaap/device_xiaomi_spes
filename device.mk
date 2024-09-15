2bedb8c69077f70b358737973caa9955c76ec0f2
angelomds42 <angelomds42@gmail.com>
2024-09-14 21:22:26 -0300

    spes: Build xiaomi-telephony-stub
    We need this for Ims to work properly.
    
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- device.mk
+++ device.mk
@@ -484,10 +484,12 @@ PRODUCT_PACKAGES += \
     qti_telephony_hidl_wrapper_prd.xml \
     qti-telephony-utils \
     qti_telephony_utils.xml \
-    telephony-ext
+    telephony-ext \
+    xiaomi-telephony-stub
 
 PRODUCT_BOOT_JARS += \
-    telephony-ext
+    telephony-ext \
+    xiaomi-telephony-stub
 
 # Update Engine
 PRODUCT_PACKAGES += \
