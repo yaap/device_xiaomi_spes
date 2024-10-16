0d8b1419fea498cf6c2b6e770d1cb8195576461f
angelomds42 <angelomds42@gmail.com>
2024-10-15 23:40:10 -0300

    spes: Build soundtrigger HAL
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- device.mk
+++ device.mk
@@ -100,12 +100,14 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.sounddose-vendor-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio-impl
+    android.hardware.bluetooth.audio-impl \
+   android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     audio.r_submix.default \
-    audio.usb.default
+    audio.usb.default \
+    sound_trigger.primary.bengal
 
 PRODUCT_PACKAGES += \
     libaudioroute \
