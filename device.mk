aec20b974bc803b398989772c410884bf6b5360b
angelomds42 <angelomds42@gmail.com>
2024-10-15 23:46:46 -0300

    spes: Remove offending entries
    Includes non-existent modules in PRODUCT_PACKAGES
    Offending entries:
    android.hardware.audio.sounddose-vendor-impl
    android.hardware.bluetooth.a2dp@1.0-impl
    android.hardware.bluetooth.a2dp@1.0-service
    android.hardware.sensors@2.1-service.xiaomi-multihal
    libloc_net_iface
    libloc_net_iface.vendor
    liblocdiagiface
    
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- device.mk
+++ device.mk
@@ -98,7 +98,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.audio.sounddose-vendor-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
    android.hardware.soundtrigger@2.3-impl
@@ -143,9 +142,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
 PRODUCT_PACKAGES += \
-  liba2dpoffload \
-  android.hardware.bluetooth.a2dp@1.0-impl \
-  android.hardware.bluetooth.a2dp@1.0-service
+  liba2dpoffload
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
@@ -238,10 +235,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.1.vendor \
-    android.hardware.gnss@2.1.vendor \
-    libloc_net_iface \
-    libloc_net_iface.vendor \
-    liblocdiagiface
+    android.hardware.gnss@2.1.vendor
 
 PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.0.vendor \
