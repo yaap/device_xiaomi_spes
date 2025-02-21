5f65f049fa4d888141e42cc23d63f1bcdb3fc8ec
Guidix <guidixm@gmail.com>
2025-02-21 09:24:48 +0200

    spes: Build Miui Camera
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -172,6 +172,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
+$(call inherit-product-if-exists, vendor/xiaomi/spes-miuicamera/miuicamera.mk)
+
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
