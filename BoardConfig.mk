ddf0bb1134edbb31590f827e26edb6b8348fd061
Guidix <guidixm@gmail.com>
2025-03-13 19:02:37 +0200

    spes: Build Kernel With Neutron Clang
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,6 +121,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := vendor/spes-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/spes
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := neutron
 TARGET_LINUX_KERNEL_VERSION := 4.19
 
 # Lineage Health
