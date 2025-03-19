31efccc9c0581b14443ba39087fe8a123c99e2bd
Guidix <guidixm@gmail.com>
2025-03-19 17:01:12 +0200

    Revert "spes: Remove OMX"
    This reverts commit c47f884f05517d09f81dfff24b1f41993ec65ca5.

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -55,6 +55,7 @@ function lib_to_package_fixup_vendor_variants() {
             echo "${1}_vendor"
             ;;
             libgrallocutils | \
+            libOmxCore | \
             libwpa_client) ;;
         *)
             return 1
